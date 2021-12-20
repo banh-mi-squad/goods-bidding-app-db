b6f0cc607acba89d50a08aac0bafec707545629f
Ken Do <khuong.a.do@gmail.com>
2021-12-20 21:24:33 +0700

    first commit

---
+++ sql/V1__Create_person_table.sql
@@ -0,0 +1,5 @@
+/* This is just an example , feel free to remove it*/
+create table PERSON (
+    ID int not null,
+    NAME varchar(100) not null
+);
---
+++ sql/V2__Add_people.sql
@@ -0,0 +1,4 @@
+/* This is just an example , feel free to remove it*/
+insert into PERSON (ID, NAME) values (1, 'Axel');
+insert into PERSON (ID, NAME) values (2, 'Mr. Foo');
+insert into PERSON (ID, NAME) values (3, 'Ms. Bar');
